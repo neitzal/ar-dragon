851446e76f985c6dc8a091c29741c8eb15d4b296
Alexander Neitz <alexander.neitz@gmail.com>
2016-05-11 11:35:21 +0200

    Fix left and right buttons

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -5,15 +5,34 @@ public class GameManagement : MonoBehaviour {
 
 	public GameObject snakePrefab;
 	public GameObject resetButton;
+	public GameObject buttonLeft;
+	public GameObject buttonRight;
 
 	private GameObject snake;
-
+	private SnakeMovement snakeMovement;
 
 	public void ResetGame() {
 		if (snake != null) {
 			Destroy(snake);
 		}
 		snake = (GameObject) Instantiate(snakePrefab, Vector3.zero, Quaternion.identity);
+		snakeMovement = snake.GetComponent<SnakeMovement>();
+	}
+
+	public void StartTurningLeft() {
+		snakeMovement.StartTurningLeft();
+	}
+
+	public void StopTurningLeft() {
+		snakeMovement.StopTurningLeft();
+	}
+
+	public void StartTurningRight() {
+		snakeMovement.StartTurningRight();
+	}
+
+	public void StopTurningRight() {
+		snakeMovement.StopTurningRight();
 	}
 
 	public void GameOver() {
