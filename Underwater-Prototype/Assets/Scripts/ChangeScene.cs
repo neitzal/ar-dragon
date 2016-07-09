c5f42fac88bc332c76d2df80cec0136537b0e283
Nora <schuster_no@yahoo.de>
2016-07-09 15:54:23 +0200

    added gui and full start scene with terrain
    added assets and scenes with gui, for start scene, scene between level 1
    and 2, scene between level 2 and 3, end scene, scenes for scanning
    framemarkers

---
+++ Underwater-Prototype/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void ChangeToScene (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	
+	}
+}
---
+++ Underwater-Prototype/Assets/Scripts/Director.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+	private GameObject storyCanvas1, storyCanvas2, startCanvas;
+
+	void Awake() {
+		storyCanvas1 = GameObject.Find("StoryCanvas1");
+		storyCanvas2 = GameObject.Find("StoryCanvas2");
+		startCanvas = GameObject.Find("StartCanvas");
+	}
+
+	void Start()
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		storyCanvas1.SetActive(true);
+		storyCanvas2.SetActive(false);
+		startCanvas.SetActive(false);
+	}
+
+	public void switchToSecond(){
+		storyCanvas1.SetActive(false);
+		storyCanvas2.SetActive(true);
+	}
+
+	public void switchToStart(){
+		storyCanvas2.SetActive(false);
+		startCanvas.SetActive(true);
+	}
+
+}
