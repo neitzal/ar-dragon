60b16c3aac674e4566f179fe7802a548a5019687
Nora <schuster_no@yahoo.de>
2016-07-10 21:51:16 +0200

    added intro and outro scenes

---
+++ Underwater-Prototype/Assets/Scripts/EndSequence.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSequence : MonoBehaviour {
+
+	public string sceneToChangeTo;
+
+	void OnCollisionEnter(Collision col){
+
+		if (col.gameObject.name == "CubeCollider") {
+
+			Application.LoadLevel (sceneToChangeTo);
+
+		}
+
+	}
+}
