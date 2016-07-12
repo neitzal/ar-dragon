61e49f4eb44be1a33029b3d542a0b24f9a49f72c
Lin Xu <lin.xu@uni-ulm.de>
2016-07-12 09:41:11 +0200

    intro cam Move

---
+++ Underwater-Prototype/Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCamera : MonoBehaviour {
+	private Camera cam; 
+	public float turnSpeed = 100f; 
+	public Transform target;
+	public float forwardSpeed = 10f; 
+
+	void Awake(){
+		cam = GetComponentInChildren<Camera> (); 
+	}
+	// Update is called once per frame
+	void Update () {
+		// cam.transform.localPosition = new Vector3 (); 
+		transform.position = target.position;
+		transform.Rotate(-Vector3.up * Time.smoothDeltaTime * turnSpeed);  
+		cam.transform.localPosition -= cam.transform.localPosition.normalized * forwardSpeed * Time.smoothDeltaTime; //(transform.position - cam.transform.position).normalized * forwardSpeed * Time.smoothDeltaTime;
+		// cam.transform.Translate((transform.position-cam.transform.position).normalized*forwardSpeed*Time.smoothDeltaTime); 
+	}
+}
